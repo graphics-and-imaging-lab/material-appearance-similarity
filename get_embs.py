7db0acf0314041a25f7c1f838149bde546cc1b3b
mlagunas <mlagunas94@gmail.com>
2019-10-15 16:09:04 +0200

    Add umap img

--- get_embs.py
+++ get_embs.py
@@ -39,7 +39,7 @@ def get_embeddings(model, imgs):
 if __name__ == '__main__':
     weights_path = './model_best.pth.tar'
     imgs_path = './havran1_stpeters_256x256_LDR'
-    embs_path = './embs.mat'
+    embs_path = './embs.mat'  # we will store the obtained feature vectors in this path
 
     model = utils.load_model(weights_path)
     imgs, img_paths = utils.load_imgs(imgs_path)
