ddea7befbc22b4841cd2b4a9e390ff2d7202468f
mlagunas <mlagunas94@gmail.com>
2020-03-02 12:45:55 +0100

    update get_embs

--- get_embs.py
+++ get_embs.py
@@ -38,21 +38,22 @@ def get_embeddings(model, imgs):
 
 if __name__ == '__main__':
 
-    # get weights path and model type
+    # get weights path to load in the model, path to store embeddings and if the
+    # model is Efficient Net.
     # weights, is_efficientnet = 'data/resnet_similarity_best.pth.tar', False
     # weights, is_efficientnet = 'data/resnet_classification_best.pth.tar', False
-    weights, is_efficientnet = 'data/efficientnet_similarity_best.pth.tar', True
+    weights_path, embs_path, is_efficientnet = \
+        'data/efficientnet_similarity_best.pth.tar', \
+        'data/efficientnet_similarity_embs_havran_ennis', \
+        True
 
     # path to the images we will extract their feature vectors
     imgs_path = 'data/havran1_ennis_298x298_LDR'
 
-    # we will store the obtained feature vectors in this path
-    embs_path = 'data/embs_similarity_resnet_havran_ennis.mat'
-
     if is_efficientnet:
-        model = utils.load_model_ef(weights)
+        model = utils.load_model_ef(weights_path)
     else:
-        model = utils.load_model(weights)
+        model = utils.load_model(weights_path)
 
     imgs, img_paths = utils.load_imgs(imgs_path)
     embs = get_embeddings(model, imgs)
